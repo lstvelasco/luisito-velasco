3ca66dadebd4ac32b2b9450f49c3c984e98e3370
Luisito Velasco <lstvelasco@gmail.com>
2024-09-18 06:13:23 +0800

    Stick to dark and light theme to support older browsers

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,6 @@ export default {
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
-    themes: ["corporate", "night"],
+    themes: ["dark", "light"],
   },
 };
